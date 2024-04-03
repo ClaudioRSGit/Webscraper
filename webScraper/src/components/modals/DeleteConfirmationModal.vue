02e3968c88c0846e29cf15d939bbe0bf99c65c3f
ClaudioRSGit <claudio.silva.t0123173@edu.atec.pt>
2024-04-03 12:28:25 +0100

    wishlist modal added to delete confirmation and edit div

--- webScraper/src/components/modals/DeleteConfirmationModal.vue
+++ webScraper/src/components/modals/DeleteConfirmationModal.vue
@@ -7,7 +7,7 @@
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            Tem certeza de que deseja excluir esta wishlist?
+            Tem certeza de que deseja apagar esta wishlist?
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
